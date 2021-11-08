f9428775d860133c6702dc7e395b4ea503792e48
Juan Pablo Ibañez <ing.juanpablo@gmail.com>
2021-11-08 09:17:40 -0700

    Add Themes

--- AlternateRowColorSample/App.xaml.cs
+++ AlternateRowColorSample/App.xaml.cs
@@ -1,32 +1,34 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace AlternateRowColorSample
 {
-	public partial class App : Application
-	{
-		public App()
-		{
-			InitializeComponent();
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
 
-			MainPage = new MainPage();
-		}
+            App.Current.Resources.MergedDictionaries.Clear();
+            Current.Resources.MergedDictionaries.Add(new DarkTheme());
 
-		protected override void OnStart()
-		{
-			// Handle when your app starts
-		}
+            MainPage = new MainPage();
+        }
 
-		protected override void OnSleep()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnResume()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
--- AlternateRowColorSample/MainPage.xaml.cs
+++ AlternateRowColorSample/MainPage.xaml.cs
@@ -1,27 +1,48 @@
 ﻿using System.Collections.Generic;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace AlternateRowColorSample
 {
-	public partial class MainPage : ContentPage
-	{
-		public List<string> Items { get; set; } = new List<string>()
-		{
-			"R2-D2",
-			"3CPO",
-			"Luke Skywalker",
-			"Darth Vader",
-			"Han Solo",
-			"Chewbacca",
-			"Yoda",
-			"Obi-Wan Kenobi"
-		};
+    public partial class MainPage : ContentPage
+    {
+        public List<string> Items { get; set; } = new List<string>()
+        {
+            "R2-D2",
+            "3CPO",
+            "Luke Skywalker",
+            "Darth Vader",
+            "Han Solo",
+            "Chewbacca",
+            "Yoda",
+            "Obi-Wan Kenobi"
+        };
 
-		public MainPage()
-		{
-			InitializeComponent();
+        public MainPage()
+        {
+            InitializeComponent();
 
-			BindingContext = this;
-		}
-	}
+            BindingContext = this;
+        }
+
+        private void Switch_OnToggled(object sender, ToggledEventArgs e)
+        {
+            Switch switchControl = sender as Switch;
+
+            ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+            if (mergedDictionaries != null)
+            {
+                mergedDictionaries.Clear();
+
+                if (switchControl.IsToggled)
+                {
+                    mergedDictionaries.Add(new DarkTheme());
+                }
+                else
+                {
+                    mergedDictionaries.Add(new LightTheme());
+                }
+            }
+        }
+    }
 }
